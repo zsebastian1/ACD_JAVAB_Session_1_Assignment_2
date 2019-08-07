2a081bd15f293587b8299a44cc291ba56754ebed
zsebastian1 <44416197+zsebastian1@users.noreply.github.com>
2019-08-07 14:43:16 -0400

    Add files via upload

---
+++ Assignment1_2.java
@@ -0,0 +1,15 @@
+package ACADAssignments;
+
+public class Assignment1_2 {
+
+	public static void main(String[] args) {
+
+		int x = 10;
+		int y = 5;
+		
+		int sum = x - (-y);
+		
+		System.out.println(x + " + " + y + " = " + sum);
+	}
+
+}
